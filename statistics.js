17288fddb063f64fb01da46927b914da0a6849ab
Chris <ulrich@tcs-germany.de>
2018-01-15 10:14:41 +0100

    first working Version

--- statistics.js
+++ statistics.js
@@ -7,4 +7,39 @@ dhtmlxEvent(window,"load",function(){
   Statistics.Grid.attachHeader("#text_filter,#text_filter");
   Statistics.Grid.setInitWidths('*,100');
   Statistics.Grid.init();
+  Statistics.OnCreateForm = function(aForm) {
+    aForm.Tabs.addTab(
+    "content",       // id
+    "Inhalt",    // tab text
+    null,       // auto width
+    null,       // last position
+    false,      // inactive
+    true);
+    aForm.Tabs.tabs("content").attachHTMLString('<div id="NoContent"><p><br>keine Reports vorhanden</p></div>');
+    aForm.Tabs.tabs("content").setActive();
+    aForm.Tabs.tabs("content").progressOn();
+    var bURL = '/'+aForm.TableName+'/by-id/'+aForm.Id+'/reports/.json';
+    if (window.LoadData(bURL,function(aData){
+      console.log("Report contents loaded");
+      try {
+        if ((aData)&&(aData.xmlDoc))
+        var aData2;
+        var aID;
+        if (aData.xmlDoc.responseText != '')
+          aData2 = JSON.parse(aData.xmlDoc.responseText);
+        if (aData2) {
+          for (var i = 0; i < aData2.length; i++) {
+            var aName = aData2[i].name.split('.')[0];
+            if (aData2[i].name.split('.')[aData2[i].name.split('.').length - 1] == 'pdf') {
+              aForm.Tabs.tabs("content").attachURL(GetBaseUrl()+'/'+aForm.TableName+'/by-id/'+aForm.Id+'/reports/'+aData2[i].name);
+              break;
+            }
+          }
+        }
+      } catch(err) {
+        aForm.Tabs.tabs("content").progressOff();
+      }
+      aForm.Tabs.tabs("content").progressOff();
+    })==true);
+  }
 });
