b4730683cacf0f1b4014f76dd5589000fc816072
Chris <ulrich@tcs-germany.de>
2018-06-13 07:40:47 +0200

    actual stand

--- statistics.js
+++ statistics.js
@@ -1,8 +1,14 @@
-﻿rtl.module("statistics",["System","Classes","Avamm","webrouter"],function () {
+﻿rtl.module("statistics",["System","Classes","Avamm","webrouter","AvammForms"],function () {
   "use strict";
   var $mod = this;
+  this.List = null;
   this.ShowStatistics = function (URl, aRoute, Params) {
+    var aParent = undefined;
     pas.System.Writeln("Statistics should be shown");
+    if (!($mod.List != null)) {
+      aParent = pas.Avamm.GetAvammContainer();
+      $mod.List = pas.AvammForms.TAvammListForm.$create("Create$1",[aParent,"statistics"]);
+    };
   };
   $mod.$resourcestrings = {strReports: {org: "Berichte"}};
   $mod.$init = function () {
--- statistics.lpr
+++ statistics.lpr
@@ -1,12 +1,22 @@
 library statistics;
-  uses js, web, classes, Avamm, webrouter;
+  uses js, web, classes, Avamm, webrouter, AvammForms;
 
 resourcestring
   strReports             = 'Berichte';
 
+var
+  List : TAvammListForm;
+
 Procedure ShowStatistics(URl : String; aRoute : TRoute; Params: TStrings);
+var
+  aParent: JSValue;
 begin
   writeln('Statistics should be shown');
+  if not Assigned(List) then
+    begin
+      aParent := GetAvammContainer();
+      List := TAvammListForm.Create(aParent,'statistics');
+    end;
 end;
 
 initialization
