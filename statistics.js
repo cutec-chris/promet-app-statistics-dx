7fde9436c233ff76f9b9de4fa62713583a5f3a29
Chris <ulrich@tcs-germany.de>
2018-06-22 15:19:34 +0200

    PDF loading works

--- statistics.js
+++ statistics.js
@@ -1,10 +1,117 @@
-﻿rtl.module("statistics",["System","JS","Web","Classes","Avamm","webrouter","AvammForms"],function () {
+﻿rtl.module("statistics",["System","JS","Web","Classes","Avamm","webrouter","AvammForms","dhtmlx_base","dhtmlx_form","SysUtils"],function () {
   "use strict";
   var $mod = this;
+  rtl.createClass($mod,"TStatisticsForm",pas.AvammForms.TAvammForm,function () {
+    this.$init = function () {
+      pas.AvammForms.TAvammForm.$init.call(this);
+      this.ContentForm = null;
+    };
+    this.$final = function () {
+      this.ContentForm = undefined;
+      pas.AvammForms.TAvammForm.$final.call(this);
+    };
+    this.DoLoadData = function () {
+      this.CreateForm();
+      this.DoOpen();
+      pas.AvammForms.TAvammForm.DoLoadData.apply(this,arguments);
+    };
+    this.CreateForm = function () {
+      var Self = this;
+      function ToolbarButtonClick(id) {
+        if (id === "execute") {
+          Self.DoExecute();
+        };
+      };
+      Self.Tabs.addTab("content",rtl.getResStr(pas.statistics,"strContent"),100,0,true,false);
+      Self.Tabs.cells("content").hide();
+      Self.Toolbar.addButton("execute",0,rtl.getResStr(pas.statistics,"strExecute"),"fa fa-pie-chart","fa fa-pie-chart");
+      Self.Toolbar.attachEvent("onClick",ToolbarButtonClick);
+      Self.ContentForm = Self.Form;
+    };
+    this.DoOpen = function () {
+    };
+    this.DoExecute = function () {
+      var Self = this;
+      function DoShowPDF(aValue) {
+        var Result = undefined;
+        function PDFIsLoaded() {
+          var aFrame = null;
+          var aRequest = null;
+          aFrame = Self.Tabs.cells("content").getFrame();
+          aRequest = aValue;
+          Self.Tabs.cells("content").show();
+          Self.Layout.progressOff();
+          try {
+            aFrame = aFrame.contentWindow;
+            var reader = new FileReader();
+            reader.addEventListener('loadend', function() {
+              var aPdf = aFrame.loadPdf({data:this.result});
+              aBlob = null;
+              reader = null;
+            });
+            aBlob = null;
+            var aBlob = new Blob([aRequest.response], {type: "application/octet-stream"})
+            reader.readAsArrayBuffer(aBlob);
+          } catch(err) {
+            console.log(err.message);
+            dhtmlx.message({
+              type : "error",
+              text: err.message,
+              expire: 3000
+            });
+          };
+        };
+        var $with1 = Self.Tabs.cells("content");
+        $with1.attachURL("\/appbase\/pdfview.html");
+        Self.Tabs.attachEvent("onContentLoaded",PDFIsLoaded);
+        return Result;
+      };
+      function ShowLoadingError(aValue) {
+        var Result = undefined;
+        Self.Layout.progressOff();
+        dhtmlx.message(pas.JS.New(["type","error","text",aValue]));
+        return Result;
+      };
+      function DoLoadPDF(aValue) {
+        var Result = undefined;
+        var aReports = null;
+        var aName = "";
+        var aExt = "";
+        var aUrl = "";
+        var i = 0;
+        var ReportLoaded = false;
+        function AddParamToUrl(aParam) {
+          if (Self.ContentForm.getUserData(aParam,"statistics","n") == "y") aUrl = (((aUrl + "&") + aParam) + "=") + ("" + Self.ContentForm.getItemValue(aParam));
+        };
+        aReports = rtl.getObject(JSON.parse(aValue.responseText));
+        for (var $l1 = 0, $end2 = aReports.length - 1; $l1 <= $end2; $l1++) {
+          i = $l1;
+          aName = "" + rtl.getObject(aReports[i])["name"];
+          aExt = pas.System.Copy(aName,pas.System.Pos(".",aName) + 1,aName.length);
+          aName = pas.System.Copy(aName,0,pas.System.Pos(".",aName) - 1);
+          if (aExt === "pdf") {
+            aUrl = (((("\/" + Self.FTablename) + "\/by-id\/") + ("" + Self.FID)) + "\/reports\/") + ("" + rtl.getObject(aReports[i])["name"]);
+            aUrl = aUrl + "?exec=1";
+            Self.ContentForm.forEachItem(AddParamToUrl);
+            pas.Avamm.LoadData(aUrl,false,"text\/json",4000).then(DoShowPDF).catch(ShowLoadingError);
+            ReportLoaded = true;
+          };
+        };
+        if (!ReportLoaded) {
+          Self.Layout.progressOff();
+          Self.Tabs.cells("content").hide();
+          dhtmlx.message(pas.JS.New(["type","warning","text",rtl.getResStr(pas.statistics,"strNoReport")]));
+        };
+        return Result;
+      };
+      Self.Layout.progressOn();
+      pas.Avamm.LoadData(((("\/" + Self.FTablename) + "\/by-id\/") + ("" + Self.FID)) + "\/reports\/.json",false,"text\/json",4000).then(DoLoadPDF).catch(ShowLoadingError);
+    };
+  });
   this.Statistics = null;
   this.ShowStatistic = function (URl, aRoute, Params) {
     var aForm = null;
-    aForm = pas.AvammForms.TAvammForm.$create("Create$1",[pas.AvammForms.TAvammFormMode.fmWindow,"statistics",Params.GetValue("Id")]);
+    aForm = $mod.TStatisticsForm.$create("Create$1",[pas.AvammForms.TAvammFormMode.fmWindow,"statistics",Params.GetValue("Id")]);
   };
   this.ShowStatistics = function (URl, aRoute, Params) {
     var aParent = null;
@@ -20,9 +127,8 @@
     };
     $mod.Statistics.Show();
   };
-  $mod.$resourcestrings = {strReports: {org: "Berichte"}};
+  $mod.$resourcestrings = {strReports: {org: "Berichte"}, strContent: {org: "Inhalt"}, strExecute: {org: "Ausführen"}, strNoReport: {org: "kein Bericht verfügbar !"}};
   $mod.$init = function () {
-    pas.System.Writeln("Hello World from Statistics...");
     pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.statistics,"strReports"),"statistics",$mod.ShowStatistics);
     pas.webrouter.Router().RegisterRoute("\/statistics\/by-id\/:Id\/",$mod.ShowStatistic,false);
   };
--- statistics.lpr
+++ statistics.lpr
@@ -1,19 +1,34 @@
 library statistics;
-  uses js, web, classes, Avamm, webrouter, AvammForms;
+  uses js, web, classes, Avamm, webrouter, AvammForms, dhtmlx_base,
+    dhtmlx_form,SysUtils;
+
+type
+
+  { TStatisticsForm }
+
+  TStatisticsForm = class(TAvammForm)
+  protected
+    ContentForm : TDHTMLXForm;
+    procedure DoLoadData; override;
+    procedure CreateForm;
+    procedure DoOpen;
+    procedure DoExecute;
+  end;
 
 resourcestring
   strReports             = 'Berichte';
+  strContent             = 'Inhalt';
+  strExecute             = 'Ausführen';
+  strNoReport            = 'kein Bericht verfügbar !';
 
 var
   Statistics : TAvammListForm = nil;
-
 Procedure ShowStatistic(URl : String; aRoute : TRoute; Params: TStrings);
 var
   aForm: TAvammForm;
 begin
-  aForm := TAvammForm.Create(fmWindow,'statistics',Params.Values['Id']);
+  aForm := TStatisticsForm.Create(fmWindow,'statistics',Params.Values['Id']);
 end;
-
 Procedure ShowStatistics(URl : String; aRoute : TRoute; Params: TStrings);
 var
   aParent: TJSHTMLElement;
@@ -31,9 +46,123 @@ begin
     end;
   Statistics.Show;
 end;
+{ TStatisticsForm }
+procedure TStatisticsForm.DoLoadData;
+begin
+  CreateForm;
+  DoOpen;
+  inherited;
+end;
+procedure TStatisticsForm.CreateForm;
+  procedure ToolbarButtonClick(id : string);
+  begin
+    if (id='execute') then
+      begin
+        DoExecute;
+      end
+    ;
+  end;
+begin
+  Tabs.addTab('content',strContent,100,0,true,false);
+  Tabs.cells('content').hide;
+  Toolbar.addButton('execute',0,strExecute,'fa fa-pie-chart','fa fa-pie-chart');
+  Toolbar.attachEvent('onClick', @ToolbarButtonClick);
+  ContentForm := Form;
+end;
+procedure TStatisticsForm.DoOpen;
+begin
+
+end;
+procedure TStatisticsForm.DoExecute;
+  function DoShowPDF(aValue: TJSXMLHttpRequest): JSValue;
+    procedure PDFIsLoaded;
+    var
+      aFrame: TJSWindow;
+      aRequest: TJSXMLHttpRequest;
+    begin
+      aFrame := Tabs.cells('content').getFrame;
+      aRequest := aValue;
+      Tabs.cells('content').show;
+      Layout.progressOff;
+      asm
+        try {
+          aFrame = aFrame.contentWindow;
+          var reader = new FileReader();
+          reader.addEventListener('loadend', function() {
+            var aPdf = aFrame.loadPdf({data:this.result});
+            aBlob = null;
+            reader = null;
+          });
+          aBlob = null;
+          var aBlob = new Blob([aRequest.response], {type: "application/octet-stream"})
+          reader.readAsArrayBuffer(aBlob);
+        } catch(err) {
+          console.log(err.message);
+          dhtmlx.message({
+            type : "error",
+            text: err.message,
+            expire: 3000
+          });
+        }
+      end;
+    end;
+  begin
+    with Tabs.cells('content') do
+      attachURL('/appbase/pdfview.html');
+    Tabs.attachEvent('onContentLoaded',@PDFIsLoaded);
+  end;
+  function ShowLoadingError(aValue: JSValue): JSValue;
+  begin
+    Layout.progressOff;
+    dhtmlx.message(js.new(['type','error',
+                           'text',aValue]));
+  end;
+  function DoLoadPDF(aValue: TJSXMLHttpRequest): JSValue;
+  var
+    aReports: TJSArray;
+    aName,aExt, aUrl: String;
+    i: Integer;
+    ReportLoaded: Boolean = False;
+
+    procedure AddParamToUrl(aParam : string);
+    begin
+      if (ContentForm.getUserData(aParam,'statistics', 'n') = 'y') then
+        aUrl := aUrl+'&'+aParam+'='+string(ContentForm.getItemValue(aParam));
+    end;
+  begin
+    aReports := TJSArray(TJSJSON.parse(aValue.responseText));
+    //Build Params
+    for i := 0 to aReports.length-1 do
+      begin
+        aName := string(TJSObject(aReports[i]).Properties['name']);
+        aExt := copy(aName,pos('.',aName)+1,length(aName));
+        aName := copy(aName,0,pos('.',aName)-1);
+        if aExt = 'pdf' then
+          begin
+            aUrl := '/'+TableName+'/by-id/'+string(Id)+'/reports/'+string(TJSObject(aReports[i]).Properties['name']);
+            aUrl := aUrl+'?exec=1';
+            ContentForm.forEachItem(@AddParamToUrl);
+            //Load PDF
+            LoadData(aUrl)._then(TJSPromiseResolver(@DoShowPDF))
+                          .catch(@ShowLoadingError);
+            ReportLoaded := True;
+          end;
+      end;
+    if not ReportLoaded then
+      begin
+        Layout.progressOff;
+        Tabs.cells('content').hide;
+        dhtmlx.message(js.new(['type','warning',
+                               'text',strNoReport]));
+      end;
+  end;
+begin
+  Layout.progressOn;
+  LoadData('/'+Tablename+'/by-id/'+string(Id)+'/reports/.json')._then(TJSPromiseresolver(@DoLoadPDF))
+                                                               .catch(@ShowLoadingError);
+end;
 
 initialization
-  writeln('Hello World from Statistics...');
   RegisterSidebarRoute(strReports,'statistics',@ShowStatistics);
   Router.RegisterRoute('/statistics/by-id/:Id/',@ShowStatistic);
 end.
